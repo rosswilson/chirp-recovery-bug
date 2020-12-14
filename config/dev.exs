6f9a5e75c40164fb063768fb3d269b898fcb3509
Ross Wilson <me@rosswilson.co.uk>
2020-12-14 22:58:02 +0000

    Scaffolded basic post live view

--- config/dev.exs
+++ config/dev.exs
@@ -18,7 +18,7 @@ config :chirp, Chirp.Repo,
 config :chirp, ChirpWeb.Endpoint,
   http: [port: 4000],
   debug_errors: true,
-  code_reloader: true,
+  code_reloader: false,
   check_origin: false,
   watchers: [
     node: [
---
+++ lib/chirp/timeline/post.ex
@@ -0,0 +1,19 @@
+defmodule Chirp.Timeline.Post do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "posts" do
+    field :body, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(post, attrs) do
+    post
+    |> cast(attrs, [:body])
+    |> validate_required([:body])
+  end
+end
